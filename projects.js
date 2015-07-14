e71cb86210c469330fff2403ca3ca02c84a804a0
wonkler <thewonkler@gmail.com>
2015-07-13 21:26:05 -0600

    Wrote core classes and implemented waterfall timescale

--- projects.js
+++ projects.js
@@ -210,3 +210,166 @@ var Note = function(content) {
     this.content = content;
     this.timestamp = getTimestamp();
 }
+
+
+
+///////////////////////////////////////////////
+// Waterfall Navigation
+///////////////////////////////////////////////
+
+$(document).ready(function() {
+    var project1 = $('<div class="waterfall-large-project"><p>Project 1</p></div>');
+    var project2 = $('<div class="waterfall-large-project"><p>Project 2</p></div>');
+    var project3 = $('<div class="waterfall-large-project"><p>Project 3</p></div>');
+    var project4 = $('<div class="waterfall-large-project"><p>Project 4</p></div>');
+    var project5 = $('<div class="waterfall-large-project"><p>Project 5</p></div>');
+    
+    project1.css({
+        position: 'relative',
+        left: '0%',
+        width: '12%',
+        height: '30px',
+        margin: '8px',
+        backgroundColor: '#C3A5FF'
+    });
+    
+    project2.css({
+        position: 'relative',
+        left: '2%',
+        width: '18%',
+        height: '30px',
+        margin: '8px',
+        backgroundColor: '#9B96E8'
+    });
+    
+    project3.css({
+        position: 'relative',
+        left: '5%',
+        width: '25%',
+        height: '30px',
+        margin: '8px',
+        backgroundColor: '#B2C1FF'
+    });
+    
+    project4.css({
+        position: 'relative',
+        left: '10%',
+        width: '40%',
+        height: '30px',
+        margin: '8px',
+        backgroundColor: '#96BCE8'
+    });
+    
+    project5.css({
+        position: 'relative',
+        left: '26%',
+        width: '50%',
+        height: '30px',
+        margin: '8px',
+        backgroundColor: '#A5E6FF'
+    });
+    
+    $('.waterfall-large').append(project1, project2, project3, project4, project5);
+    
+    $('#save-new-project').click(function() {
+        var name = $('#project-name').val();
+        var startDate = $('#project-start-date').val();
+        var dueDate = $('#project-due-date').val();
+
+//        return new Project(name, startDate, dueDate);
+        console.log('New Project Saved!', name, startDate, dueDate);
+    });
+    
+    $('#cancel-new-project').click(function() {
+        console.log('New Project Canceled =(');
+    });
+    
+}); // End Ready
+
+
+
+
+///////////////////////////////////////////////
+// D3 Waterfall Visualization
+///////////////////////////////////////////////
+
+/////////////////////////
+// Test Data 
+/////////////////////////
+var testData = [
+    { name: 'Design Website', startDate: '2014-07-01', dueDate: '2015-08-15'},
+    { name: 'Logo Mockup', startDate: '2015-07-15', dueDate: '2015-08-15'},
+    { name: 'Build Plugin', startDate: '2015-07-15', dueDate: '2015-08-15'},
+    { name: 'Write Script', startDate: '2015-07-15', dueDate: '2015-08-15'},
+    { name: 'Redesign Database', startDate: '2015-07-15', dueDate: '2015-08-15'}
+];
+
+
+
+/////////////////////////
+// Visualization 
+/////////////////////////
+
+// Layout variables
+var width = 1200;
+var height = 500;
+var sidePadding = 0;
+var topPadding = 0;
+
+// Create a selection for the svgContainer
+var svgContainer = d3.selectAll(".waterfall-large-svg")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .attr("class", "svg-container");
+
+
+// Format the dates and times
+var dateFormat = d3.time.format("%Y-%m-%d");
+var timeFormat = d3.time.format('%b %e, %Y');
+
+// Return minimum and maximum dates
+var min = d3.min(testData, function(d) { 
+    return dateFormat.parse(d.startDate);
+});
+var max = d3.max(testData, function(d) { 
+    return dateFormat.parse(d.dueDate);
+});
+
+// Pass the min and max dates, create our time scale
+var timeScale = d3.time.scale().domain([min, max]).range([40, width - 40]);
+
+
+// Draw the Timeline Axis and Grid
+var drawAxis = function(sidePadding, topPadding, width, height) {
+
+    var interval = d3.time.day;
+    var tickWidth = 100; // Set maximum tick width
+    var maxTicks = width / tickWidth;
+    var skip = Math.ceil(interval.range(min, max).length / maxTicks); // Reference our dynamic min and max
+    
+    var xAxis = d3.svg.axis()
+        .scale(timeScale)
+        .orient('bottom')
+        .tickValues(interval.range(min, max).filter(function(d, i) {
+            return !(i % skip);
+        }))
+        .tickSize(-height + topPadding + 20, 0, 0)
+        .tickFormat(timeFormat);
+    
+    var ticks = svgContainer.append('g')
+        .attr('class', 'grid')
+        .attr('transform', 'translate(' + sidePadding + ', ' + (height - 50) + ')')
+        .call(xAxis)
+        .selectAll("text")  
+        .style("text-anchor", "middle")
+        .attr("fill", "#000")
+        .attr("stroke", "none")
+        .attr("font-size", 10);
+    
+    return ticks;
+}
+
+// Finally, draw our axis!
+drawAxis(0, 0, width, height);
+
